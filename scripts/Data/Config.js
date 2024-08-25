d53c8ca12ba523fb1f8cbfefb6c4fb8571a7656f
NKPGAMER <nguyenkimphuc2909@gmail.com>
2024-08-25 12:04:55 +0700

    up

--- scripts/Data/Config.js
+++ scripts/Data/Config.js
@@ -4,17 +4,17 @@ export default (config => {
     'owner': "", /* Account Name */
     'passworld': "" /* Password to access Admin rights - (If left blank, Admin rights will be automatically granted to the owner) */
   };
-  
+
   /* Contains information about language and region  */
   config['region'] = {
     'area': "vi-VN", /* Your region's time format */
     'language': "vi-VN" /* Your language id - (If not supported, default will be Vietnamese) */
   };
-  
+
   config['customCommand'] = {
     'enable': true, /* Allow the world to use custom commands */
-    'prefix': "./", /* Prefix to launch command */
-  }
-  
+    'prefix': ".", /* Prefix to launch command */
+  };
+
   return config;
 })({});
