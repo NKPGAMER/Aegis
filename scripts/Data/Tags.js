d38e27494eae238960cfa55a31b17f5b7c7a2446
NKPGAMER <160864873+NKPGAMER@users.noreply.github.com>
2024-08-26 16:03:54 +0700

    c

--- scripts/Data/Tags.js
+++ scripts/Data/Tags.js
@@ -1,3 +1,5 @@
-export default {
-  
-}
+export default ((tags) => {
+  // Disable feature
+  tags['breakBlock-disable'] = 'aegis:disable-breakBlock';
+  tags['placeBlock-disable'] = 'aegis:disable-place+block'
+})({})
