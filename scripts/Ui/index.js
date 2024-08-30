4f5c6d4108912c4ddc9b5d442722b2e49c6f9c26
NKPGAMER <nguyenkimphuc2909@gmail.com>
2024-08-30 18:59:03 +0700

    ui

--- scripts/Ui/index.js
+++ scripts/Ui/index.js
@@ -1,6 +1,13 @@
-import { world, system, GameMode, WeatherType } from "@minecraft/server";
+import { world, system, Player, GameMode, WeatherType } from "@minecraft/server";
 import { ActionFormData, ModalFormData, MessageFormData } from "../Modules/CustomUI";
-import { ChangeGameMode, isAdmin, isModerator, setActionBar, getAllItemStack, ItemStackToJSON, JsonToItemStack } from '../Assets/Utils';
+import { ChangeGameMode as setGameMode, isAdmin, isModerator, setActionBar, getAllItemStack, ItemStackToJSON, JsonToItemStack } from '../Assets/Utils';
+
+world.beforeEvents.itemUse.subscribe((event) => {
+  if (event.itemStack.typeId === 'aegis:menu' && event.source instanceof Player) {
+    event.cancel = true;
+    openUI(event.source);
+  }
+});
 
 const main = {
   member: new ActionFormData()
@@ -9,7 +16,10 @@ const main = {
 
   moderator: new ActionFormData(),
 
-  admin: new ActionFormData()
+  admin: (player) => {
+    const form = new ActionFormData();
+
+  }
 };
 
 function openUI(player) {
@@ -26,26 +36,54 @@ function openUI(player) {
 
 const profile = new ActionFormData()
   .back(openUI)
-  .button(Aegis.Trans('aegis.ui.infraction_history'))
+  .button(Aegis.Trans('aegis.ui.infraction_history'));
 
 const ChangeGameMode = new ActionFormData()
+  .title(Aegis.Trans('aegis.ui.change_gamemode'))
   .back((player) => isAdmin(player) ? Tool : main.moderator(player))
-  .button(Aegis.Trans('aegis.gamemode.adventure'), undefined, (player) => player.setGameMode(GameMode.adventure))
-  .button(Aegis.Trans('aegis.gamemode.survival'), undefined, (player) => player.setGameMode(GameMode.survival))
-  .button(Aegis.Trans('aegis.gamemode.creative'), undefined, (player) => player.setGameMode(GameMode.creative))
-  .button(Aegis.Trans('aegis.gamemode.spectator'), undefined, (player) => player.setGameMode(GameMode.spectator));
+  .button(Aegis.Trans('aegis.gamemode.adventure'), undefined, (player) => setGameMode(player, GameMode.adventure))
+  .button(Aegis.Trans('aegis.gamemode.survival'), undefined, (player) => setGameMode(player, GameMode.survival))
+  .button(Aegis.Trans('aegis.gamemode.creative'), undefined, (player) => setGameMode(player, GameMode.creative))
+  .button(Aegis.Trans('aegis.gamemode.spectator'), undefined, (player) => setGameMode(player, GameMode.spectator));
 
-const ChangWeather = (player) => new ModalFormData()
+const _ChangeWeather = new ModalFormData()
   .title(Aegis.Trans('aegis.ui.change_weather'))
   .dropdown(Aegis.Trans('aegis.ui.change_weather.select'), Object.keys(WeatherType).map(w => Aegis.Trans('aegis.weather.') + w))
-  .textField(Aegis.Trans('aegis.ui.change_weather.duration'), 'ticks')
-  .show(player)
-  .then(res => {
-    if(res.canceled) return;
+  .textField(Aegis.Trans('aegis.ui.change_weather.duration'), 'ticks');
 
-    const weather = WeatherType[Object.keys(WeatherType)[res.formValues[0]]]
-    const duration = Number(res.formValues[1]) || Math.randomInt(1200, 36000);
+function ChangeWeather(player) {
+  _ChangeWeather.show(player)
+    .then(res => {
+      if (res.canceled) return;
+      const weather = WeatherType[Object.keys(WeatherType)[res.formValues[0]]];
+      const duration = Number(res.formValues[1]) || Math.randomInt(1200, 36000);
 
-    world.getDimension('overworld').setWeather(weather, duration);
-    player.tell('')
-  })
+      world.getDimension('overworld').setWeather(weather, duration);
+      player.tell(Aegis.Trans('aegis.ui.change_weather.complete')?.replace('<weather>', Aegis.Trans('aegis.weather.' + weather)));
+    })
+    .catch((error) => {
+      error.function = ChangWeather.name;
+      console.error(error?.toString() || error);
+    });
+}
+
+const timeType = ['sunrise', 'day', 'noon', 'sunset', 'night', 'midnight'];
+const _ChangeTime = new ModalFormData()
+  .title(Aegis.Trans('aegis.ui.change_time'))
+  .dropdown(Aegis.Trans('aegis.ui.change_time.select'), timeType.map(t => Aegis.Trans(`aegis.time.${t}`)));
+
+function ChangeTime(player) {
+  _ChangeTime.show(player)
+    .then(res => {
+      if (res.canceled) return;
+      world.getDimension('overword').runCommand(`time set ${timeType[res.formValues[0]]}`);
+      player.tell(Aegis.Trans('aegis.change_time.complete')?.replace('<time>', Aegis.Trans(`aegis.time.${timeType[res.formValues[0]]}`)));
+    });
+}
+
+const worldTool = new ActionFormData()
+  .title(Aegis.Trans('aegis.ui.world_tool'))
+  .back(openUI)
+  .button(Aegis.Trans('aegis.ui.change_gamemode'), null, ChangeGameMode.show)
+  .button(Aegis.Trans('aegis.ui.change_weather'), null, ChangeWeather)
+  .button(Aegis.Trans('aegis.ui.change_time'), null, ChangeTime);
