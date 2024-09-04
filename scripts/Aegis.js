2406ea00f2245904499f753cf91be8002d0c5da5
NKPGAMER <nguyenkimphuc2909@gmail.com>
2024-09-04 19:26:31 +0700

    update

--- scripts/Aegis.js
+++ scripts/Aegis.js
@@ -1,68 +1,84 @@
-import { world } from '@minecraft/server';
+const startTime = Date.now();
+console.warn('Preparing...');
+
+import { world, Player } from '@minecraft/server';
 import { Database } from './Assets/Database';
 import { MemoryCache } from './Assets/MemoryCache';
 
-globalThis['Aegis'] = (aegis => {
+globalThis.Aegis = (() => {
   const AegisPrefix = '§7[§eAegis§7]§r ';
+  const localOpId = new Set([ '-4294967295', '-206158430207' ]);
+  const aegis = {};
 
-  aegis['sendMessage'] = (value) => {
-    const msg = typeof value === 'string'
+  const formatMessage = value =>
+    typeof value === 'string'
       ? AegisPrefix + value
       : { rawtext: [{ text: AegisPrefix }, { rawtext: [value] }] };
-    world.sendMessage(msg);
+
+  aegis.sendMessage = (value, target) => {
+    const msg = formatMessage(value);
+
+    if (!target) {
+      world.sendMessage(msg);
+    } else if (target instanceof Player) {
+      target.sendMessage(msg);
+    } else if (typeof target === 'function') {
+      world.getAllPlayers().filter(target).forEach(player => player.sendMessage(msg));
+    } else if (Array.isArray(target)) {
+      target.forEach(p => p instanceof Player && p.sendMessage(msg));
+    }
   };
 
-  aegis['defaultDimension'] = world.getDimension('overworld');
+  aegis.defaultDimension = world.getDimension('overworld');
 
-  aegis['runCommand'] = (command) => aegis['defaultDimension'].runCommand(command);
+  aegis.runCommand = command => aegis.defaultDimension.runCommand(command);
 
-  aegis['runCommandAsync'] = (command) => aegis['defaultDimension'].runCommandAsync(command);
+  aegis.runCommandAsync = command => aegis.defaultDimension.runCommandAsync(command);
 
-  aegis['events'] = {
-    subscribe: EventSubscribe,
-    unsubscribe: EventUnsubscribe
-  };
+  aegis.events = { subscribe: EventSubscribe, unsubscribe: EventUnsubscribe };
 
-  aegis['ServerType'] = (() => world.getAllPlayers().filter(({ id }) => id === '-4294967295' || id === '-206158430207').length > 0 ? 'local' : 'server')();
+  aegis.ServerType = (() => {
+    
+  })() //world.getAllPlayers().some(({ id }) => ['-4294967295', '-206158430207'].includes(id)) ? 'local' : 'server';
 
-  aegis['config'] = new Database('config');
+  aegis.config = new Database('config');
 
-  aegis['Database'] = Database;
+  aegis.Database = Database;
 
-  aegis['MemoryCache'] = MemoryCache;
+  aegis.MemoryCache = MemoryCache;
 
   return aegis;
-})({});
+})();
 
 function EventSubscribe(EventType, EventId, callback, ...options) {
-  const eventType = EventType.startsWith('after')
-    ? 'afterEvents'
-    : EventType.startsWith('before')
-      ? 'beforeEvents'
-      : void 0;
-  if (!eventType) throw new Error(`Invalid event type: \"${EventType}\"`);
+  const eventType = EventType.startsWith('after') ? 'afterEvents' : EventType.startsWith('before') ? 'beforeEvents' : null;
+  if (!eventType) throw new Error(`Invalid event type: "${EventType}"`);
 
-  const event = world?.[eventType]?.[EventId]?.['subscribe'];
+  const event = world?.[eventType]?.[EventId]?.subscribe;
   if (!event) throw new Error(`Event id not found: ${EventId}`);
-  event(callback, options.length > 0 ? [...options] : void 0);
+  event(callback, options.length > 0 ? options : undefined);
 }
 
 function EventUnsubscribe(EventType, EventId, callback) {
-  const eventType = EventType.startsWith('after')
-    ? 'afterEvents'
-    : EventType.startsWith('before')
-      ? 'beforeEvents'
-      : void 0;
-  if (!eventType) throw new Error(`Invalid event type: \"${EventType}\"`);
+  const eventType = EventType.startsWith('after') ? 'afterEvents' : EventType.startsWith('before') ? 'beforeEvents' : null;
+  if (!eventType) throw new Error(`Invalid event type: "${EventType}"`);
 
-  const event = world?.[eventType]?.[EventId]?.['unsubscribe'];
+  const event = world?.[eventType]?.[EventId]?.unsubscribe;
   if (!event) throw new Error(`Event id not found: ${EventId}`);
   event(callback);
 }
 
+console.log(`Done... Total: ${Date.now() - startTime}ms`);
+
 (async () => {
-  await import('./Modules/javascript-extensions');
-  await import('./Modules/minecraft-extensions');
-  await import('./Modules/loadConfig');
-  import('./index')
-})()
+  const startImport_modules = Date.now();
+  console.warn('Import Modules...');
+  await Promise.all([
+    import('./Modules/javascript-extensions'),
+    import('./Modules/minecraft-extensions'),
+    import('./Modules/loadConfig'),
+  ]);
+  console.warn(`Done... Total: ${Date.now() - startImport_modules}ms`);
+  console.warn('Getting started...');
+  import('./index');
+})();
--- scripts/Data/Config.js
+++ scripts/Data/Config.js
@@ -1,1 +1,4 @@
-export default {}
+export default {
+  passworld: "your_password",
+  
+}
